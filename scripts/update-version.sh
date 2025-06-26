a08fb5e801823df8bcf79046897233cf4e252055
dej <david.jay@redfroggy.fr>
2025-06-26 15:06:30 +0200

    ci: add execution permission to update-version.sh

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -7,5 +7,5 @@ VERSION=$1
 # Utiliser le plugin Maven Versions pour mettre à jour le pom.xml
 mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
 
-# Facultatif : valider que le pom.xml contient bien la version
+# Facultatif : valider que le pom.xml contient bien la version.
 grep "<version>$VERSION</version>" pom.xml
